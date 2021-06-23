b3eeb2f86de8e52287b688e00fc67ccf17f7139d
Maycon Reis Rosário <mayconrr13@gmail.com>
2021-06-23 13:02:45 -0300

    add create user logic and connection with sqlite db

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import "reflect-metadata"
+import express, { json } from 'express'
+import { router } from './routes'
+
+import './database'
+
+const app = express()
+
+app.use(express.json())
+app.use(router)
+
+app.listen(3333, () => console.log('Running...'))
